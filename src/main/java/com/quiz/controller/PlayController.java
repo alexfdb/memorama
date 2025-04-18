4a13782ba51098663ea80910368de69a64079d2f
Alexander Faustino Diaz Bautista <AlexanderFaustinoDiazBautista@Gmail.com>
2025-04-18 09:31:11 +0100

    Uodate

--- src/main/java/com/quiz/controller/PlayController.java
+++ src/main/java/com/quiz/controller/PlayController.java
@@ -8,7 +8,9 @@ import com.quiz.model.QuestionModel;
 import com.quiz.model.session.SessionModel;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.text.Text;
 
 /**
@@ -36,14 +38,22 @@ public class PlayController extends ScreenController {
     @FXML
     private Button buttonReturn;
 
+    private Integer answers;
+    private Integer hits;
     private QuestionModel questionModel;
     private Optional<Question> optional;
+
+    public PlayController() {
+        this.answers = 0;
+        this.hits = 0;
+    }
 
     @FXML
     public void initialize() {
         questionModel = new QuestionModel();
         optional = questionModel.getRandomQuestion();
         textUser.setText(SessionModel.getUser().getName());
+        textPoint.setText(hits.toString());
         textQuestion.setText(optional.get().getQuestionText());
         buttonAnswer1.setText(optional.get().getAnswer1());
         buttonAnswer2.setText(optional.get().getAnswer2());
@@ -53,27 +63,49 @@ public class PlayController extends ScreenController {
 
     @FXML
     private void buttonAnswer1Click() {
-
+        checkAnswer(1);
     }
 
     @FXML
     private void buttonAnswer2Click() {
-
+        checkAnswer(2);
     }
 
     @FXML
     private void buttonAnswer3Click() {
-
+        checkAnswer(3);
     }
 
     @FXML
     private void buttonAnswer4Click() {
-        
+        checkAnswer(4);
     }
 
     @FXML
     private void buttonReturnClick() {
         levelScreen(buttonReturn);
+    }
+
+    private void checkAnswer(int selectAnswer) {
+        answers++;
+        if(optional.isPresent() && optional.get().getCorrectAnswer() == selectAnswer) {
+            hits++;
+            textPoint.setText(hits.toString());
+        }
+        if(answers == 10) {
+            Alert alert = new Alert(AlertType.INFORMATION);
+            alert.setTitle("¡Juego terminado!");
+            alert.setHeaderText(null);
+            alert.setContentText("Has acertado " + hits);
+            alert.showAndWait();
+            buttonReturnClick();
+        }
+        optional = questionModel.getRandomQuestion();
+        textQuestion.setText(optional.get().getQuestionText());
+        buttonAnswer1.setText(optional.get().getAnswer1());
+        buttonAnswer2.setText(optional.get().getAnswer2());
+        buttonAnswer3.setText(optional.get().getAnswer3());
+        buttonAnswer4.setText(optional.get().getAnswer4());
     }
 
 }
